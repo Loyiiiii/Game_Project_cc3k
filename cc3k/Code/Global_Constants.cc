b78cccb093d39d7714bb2ff2bf4ef87aa07c5e93
donaldlihe@gmail.com <donaldlihe@gmail.com>
2025-07-19 23:05:20 -0400

    changed to c++20 and added global consts

---
+++ cc3k/Code/Global_Constants.cc
@@ -0,0 +1,12 @@
+export module Global_Constants;
+
+
+export enum Race {
+    SHADE, DROW, VAMPIRE, TROLL, GOBLIN
+};
+
+
+export enum Potion_Type {
+    HEALTH_RESTORE, ATK_BOOST,DEF_BOOST, POISION_HEALTH, WOUND_ATK, WOUND_DEF
+};
+
--- cc3k/Code/PlayerCharacter.cc
+++ cc3k/Code/PlayerCharacter.cc
@@ -1,19 +1,29 @@
-export PlayerCharacter;
+import <iostream>;
+import <string>;
+import Global_Constants;
+
+
+export module PlayerCharacter;
+
 
 export class PlayerCharacter {
     // representing the PC's position on the floor
     int row; 
     int col;
-    int heath; // representing the HP
+    int health; // representing the HP
     int maxHealth; // representing the max HP
     bool has_max_health; // representing if the PC has the max health - reserved for Vampire.
     int atk; // representing the attack power
     int def; // representing the defense power
     int goldATM; // representing the gold of the PC
+    
     public: 
         void drinkPotion(Potion p); // drinks a potion and gains its effects
-        virtual PlayerCharacter(int row, int col, int health, int maxHealth, bool has_max_health, 
+        PlayerCharacter(int row, int col, int health, int maxHealth, bool has_max_health, 
             int atk, int def, int goldATM);
+        virtual ~PlayerCharacter() = default; // Virtual destructor for proper inheritance
+        
+        // Getters
         int getRow();
         int getCol();
         int getHealth();
@@ -22,6 +32,10 @@ export class PlayerCharacter {
         int getAtk();
         int getDef();
         int getGoldATM();
+        
+        // Virtual methods that races can override
+        virtual void attack();
+        virtual void takeDamage(int damage);
 };
 
 
