d6b247a594255aa427b4dce3f71510f094891bda
张凯文 <kevinzhang20031124@gmail.com>
2025-07-20 20:44:12 -0400

    created and implemented BoostDef class

---
+++ cc3k/Code/BoostDef-impl.cc
@@ -0,0 +1,3 @@
+module boost_def;
+
+BoostDef::BoostDef(): Potion{Potion_Type::DEF_BOOST, true, 5, 'P'} {}
---
+++ cc3k/Code/BoostDef.cc
@@ -0,0 +1,14 @@
+export module boost_def;
+
+import potion;
+
+export class BoostDef : public Potion {
+public:
+    BoostDef(); // Constructor for BoostDef potion
+    // Inherits from Potion, which has the type DEF_BOOST, is temporary,
+    // has an amount of 5, and a symbol 'P'.
+    // The symbol 'D' is used to represent the potion in the game.
+    // The BoostDef potion temporarily increases the player's defense power by 5.
+    // It is used to boost the player's defense in combat situations.
+    // The potion is temporary, meaning its effects will wear off after getting to a new floor
+}
