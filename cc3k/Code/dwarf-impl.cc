aac2cdb2fc0d2149629977d7ee0094b0687ac362
donaldlihe@gmail.com <donaldlihe@gmail.com>
2025-07-21 16:05:02 -0400

    kaiwen read msg

--- cc3k/Code/dwarf-impl.cc
+++ cc3k/Code/dwarf-impl.cc
@@ -2,3 +2,5 @@ module dwarf;
 
 Dwarf::Dwarf(Position pos, int HP, int Atk, int Def, bool movable, char symbol): 
     Enemy{Pos, HP, Atk, Def, movable}, symbol{'W'} {}
+
+// note: attack needs to incorporate damage calculation logic.
