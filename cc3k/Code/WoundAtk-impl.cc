e111dfc21d19f784de38bb42cfb6de3063e8b247
张凯文 <kevinzhang20031124@gmail.com>
2025-07-20 20:56:01 -0400

    implemented WoundAtk class

---
+++ cc3k/Code/WoundAtk-impl.cc
@@ -0,0 +1,3 @@
+module wound_atk;
+
+WoundAtk::WoundAtk(): Potion{Potion_Type::WOUND_ATK, true, -5, 'P'} {}
---
+++ cc3k/Code/WoundAtk.cc
@@ -0,0 +1,12 @@
+export module wound_atk;
+
+import potion;
+
+export class WoundAtk : public Potion {
+public:
+    WoundAtk(); // Constructor for WoundAtk potion
+    // Inherits from Potion, which has the type WOUND_ATK, is temporary,
+    // has an amount of -5, and a symbol 'P'.
+    // The symbol 'P' is used to represent the potion in the game.
+    // The WoundAtk potion temporarily decreases the player's attack by 5.
+}
