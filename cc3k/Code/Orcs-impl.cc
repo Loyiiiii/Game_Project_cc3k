2a5a72798f07fbe04a711cf83de55294a9062268
donaldlihe@gmail.com <donaldlihe@gmail.com>
2025-07-21 17:46:06 -0400

    orcs added

---
+++ cc3k/Code/Orcs-impl.cc
@@ -0,0 +1,42 @@
+module Orcs;
+import Enemy;
+import PlayerCharacter;
+import Global_Constants;
+
+// Orcs: 180HP, 30Atk, 25Def.
+// deals 50% more damage to goblins
+Orcs::Orcs(Position pos):
+    Enemy{pos, 180, 30, 25, true} {}
+
+void Orcs::attack(PlayerCharacter& pc) {
+    int normal_damage = calculateDamage(this->Atk, pc.getDef());
+    // check if attacking goblin.
+    if (pc.getRace() == Race::GOBLIN) {
+        // damage is done * 50%. 
+        
+        int augmented_dmg = normal_damage * 1.5;
+        pc.takeDamage(augmented_dmg);
+    }
+    else {
+        pc.takeDamage(normal_damage);
+    }
+}
+
+void Orcs::dropGold() {
+    pc.addGold(2); // need to figure out how to get the PC here. 
+}
+
+char Orcs::getSymbol() {
+    return 'O';
+}
+
+void Orcs::takeDamage(int dmg) {
+    if(HP - dmg < 0) {
+        HP = 0; // dead
+        // gold is automatically added to PC.
+        dropGold();
+    }
+    else {
+        HP -= dmg;
+    }
+}
---
+++ cc3k/Code/Orcs.cc
@@ -0,0 +1,14 @@
+export module Orcs;
+import Enemy;
+import PlayerCharacter;
+import Global_Constants;
+
+export class Orcs: public Enemy {
+    public:
+        Orcs(Position pos);
+        ~Orcs();
+        void attack(PlayerCharacter& pc) override;
+        void dropGold() override;
+        char getSymbol() override;
+        void takeDamage(int damage) override;
+};
