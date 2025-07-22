0e0dad42e810d845d2ca1f17c679329f6590457e
Loyiiiii <wsr20050616@gmail.com>
2025-07-22 18:04:49 -0400

    add unique pointer for choosing character

--- cc3k/Code/GameInit.cc
+++ cc3k/Code/GameInit.cc
@@ -2,7 +2,10 @@ export module GameInit;
 import <iostream>;
 import <fstream>;
 import <string>;
+import <memory>;
 import GamePlay;
+import PlayerCharacter;
+import Global_Constants;
 using namespace std;
 
 export enum class GameResult {
@@ -13,6 +16,7 @@ export enum class GameResult {
 
 export class GameInit {
 public:
+    virtual unique_ptr<PlayerCharacter> curr_game_PC(Position start_pos, Race start_race);
     void run();
     string getRaceEmoji(char race);
     string getRaceName(char race);
