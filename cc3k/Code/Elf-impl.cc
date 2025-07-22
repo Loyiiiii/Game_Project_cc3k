d8988249ebbfb74a8fe22d3d0d259bbcf04d275f
donaldlihe@gmail.com <donaldlihe@gmail.com>
2025-07-21 20:48:28 -0400

    remove getSymbol

--- cc3k/Code/Elf-impl.cc
+++ cc3k/Code/Elf-impl.cc
@@ -22,10 +22,6 @@ void Elf::dropGold(PlayerCharacter& pc) {
     pc.addGold(1); // Assuming Elf drops 1 gold, may change it to randomly chosen between small and normal
 }
 
-char Elf::getSymbol() {
-    return symbol;
-}
-
 void Elf::takeDamage(int damage) {
     if (HP - damage < 0) {
         HP = 0;
--- cc3k/Code/Elf.cc
+++ cc3k/Code/Elf.cc
@@ -10,7 +10,6 @@ public:
 
     void attack(PlayerCharacter& pc) override;
     void dropGold() override;
-    char getSymbol() override;
     void takeDamage(int damage) override;
     bool is_alive();
     int getAtk();
