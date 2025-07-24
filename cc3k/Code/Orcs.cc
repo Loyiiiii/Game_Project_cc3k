b6bf9d7ed2d7840c2a3f207e5060587a2835fdd7
donaldlihe@gmail.com <donaldlihe@gmail.com>
2025-07-23 21:51:14 -0400

    remove dctor

--- cc3k/Code/Orcs.cc
+++ cc3k/Code/Orcs.cc
@@ -6,7 +6,6 @@ import Global_Constants;
 export class Orcs: public Enemy {
     public:
         Orcs(Position pos);
-        ~Orcs();
         void attack(PlayerCharacter& pc) override;
         void dropGold(PlayerCharacter& pc) override;
         void takeDamage(int damage) override;
