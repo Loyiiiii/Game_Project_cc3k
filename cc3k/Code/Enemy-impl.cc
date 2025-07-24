c4ccf038c07caf528dc3918f15f34b09514a1780
donaldlihe@gmail.com <donaldlihe@gmail.com>
2025-07-23 21:24:59 -0400

    deleted space

--- cc3k/Code/Enemy-impl.cc
+++ cc3k/Code/Enemy-impl.cc
@@ -1,8 +1,9 @@
 module Enemy;
 import <cmath>;
 import Global_Constants;
-import PlayerCharacter;
+
 // Forward declaration instead of import
+class PlayerCharacter;
 
 Enemy::Enemy(Position pos, int health, int damage, int defense, bool movable, bool is_neutral, char symbol):
     pos{pos}, HP{health}, Atk{damage}, Def{defense}, movable{movable}, is_neutral{is_neutral}, symbol{symbol} {}
@@ -30,7 +31,6 @@ int Enemy::getHP() const {
 
 
 bool Enemy::isAdjacentTo(const PlayerCharacter& pc, const Position& enemy_pos) const {
-
     Position enemy_pos = this->pos; 
     // check if the player is adjacent to the enemy // within a 1-cell distance
     int row_diff = std::abs(player_pos.row - enemy_pos.row);
