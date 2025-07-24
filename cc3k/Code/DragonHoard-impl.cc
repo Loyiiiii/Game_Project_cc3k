97bd62177a6695cfc352a0e131a132a4a48dc2bb
Loyiiiii <wsr20050616@gmail.com>
2025-07-23 20:30:12 -0400

    fixed the order for make file, add header for DragonHoard

--- cc3k/Code/DragonHoard-impl.cc
+++ cc3k/Code/DragonHoard-impl.cc
@@ -1,4 +1,5 @@
 module dragon_hoard;
+import Position;
 
 DragonHoard::DragonHoard(Position position): 
 Gold{6, false}, pos{position} {}
--- cc3k/Code/DragonHoard.cc
+++ cc3k/Code/DragonHoard.cc
@@ -1,6 +1,7 @@
 export module dragon_hoard;
 
 import gold;
+import Position;
 import Global_Constants;
 
 export class DragonHoard : public Gold {
